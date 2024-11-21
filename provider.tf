b2c6f06e1f13a89dacc1e14b3021da7fd025ff82
Cody Miller <54908054+cmiller96@users.noreply.github.com>
2024-11-20 17:13:00 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,18 +8,18 @@ terraform {
   required_version = "~> 1.3"
 }
 
-provider "aws" {
-  region = var.region
-
-  assume_role {
-    session_name = "session"
-    role_arn     = "arn:aws:iam::${var.account_id}:role/${var.role}"
-  }
-
-  default_tags {
-    tags = {
-      Terraform   = "true"
-      Environment = terraform.workspace
-    }
-  }
-}
+#provider "aws" {
+#  region = var.region
+#
+#  assume_role {
+#    session_name = "session"
+#    role_arn     = "arn:aws:iam::${var.account_id}:role/${var.role}"
+#  }
+#
+#  default_tags {
+#    tags = {
+#      Terraform   = "true"
+#      Environment = terraform.workspace
+#    }
+#  }
+#}
